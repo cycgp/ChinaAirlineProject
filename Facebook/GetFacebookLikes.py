8610e1a81380fa77c5386ac8937c6464f68e1628
CHIEN,CHING-YUAN <Henry@Henry-MBP.local>
2017-03-13 02:22:27 +0800

    Multi page crawler
    Complete coding multi page crawler.

--- Facebook/GetFacebookLikes.py
+++ Facebook/GetFacebookLikes.py
@@ -8,7 +8,7 @@ def getLikesSummary(posts):
 		if 'message' in a:
 			global SID
 			print(str(SID)+':')
-			SID += 1			
+			SID += 1
 			r = requests.get('https://graph.facebook.com/'+str(a['id'])+'/?fields=created_time,message&access_token='+token)
 			post_dict = json.loads(r.text)
 			print(post_dict['created_time'])
@@ -30,10 +30,8 @@ if __name__ == '__main__':
 	graph = facebook.GraphAPI(access_token = token)
 	posts = graph.get_connections(id='119474188105563', connection_name='posts')
 	print ("\n--")
-	SID = 1 
+	SID = 1
 	getLikesSummary(posts)
 	#Get summary of all likes in a post
 	getNextPage(posts)
 	#Get next page
-	
-
--- TripAdvisor/getReview.py
+++ TripAdvisor/getReview.py
@@ -2,19 +2,33 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 import json
-import io
+import time
+import dryscrape
+
+def generateURL():
+	global URL_List
+	for page in range(0,40):
+		URL = 'https://www.tripadvisor.com.tw/Airline_Review-d8729049-Reviews-Cheap-Flights-or'+str(page)+'0-China-Airlines#Reviews'
+		#Url of China Airline in Trip Advisor
+		URL_List.append(URL)
 
 def getReviewInfo(review):
+	print(review.prettify())
 	innerBubble = review.find("div", { "class" : "innerBubble" })
 	quote = innerBubble.find("div", { "class" : "quote" }).span
 	rating = innerBubble.find("div", { "class" : "rating" }).img['alt'].encode('utf-8').split(' ')[0]
-	ratingDate = innerBubble.find("span", { "class" : "ratingDate" })['title'].encode('utf-8')
+	ratingDate = getDate(innerBubble.find("span", { "class" : "ratingDate" }))
 	comment = getTextFromTag(innerBubble.find("p", { "class" : "partial_entry" }))
-	labels = innerBubble.findAll("span", { "class" : "categoryLabel" }) #set of labels
-	area = getTextFromTag(labels[0])
-	seatClass = getTextFromTag(labels[1])
-	route = getTextFromTag(labels[2])
-	routeDetail = getRouteDetail(route)
+	try:
+		labels = innerBubble.findAll("span", { "class" : "categoryLabel" }) #set of labels
+		area = getTextFromTag(labels[0])
+		seatClass = getTextFromTag(labels[1])
+		route = getTextFromTag(labels[2])
+		routeDetail = getRouteDetail(route)
+	except:
+		area = ''
+		seatClass = ''
+		routeDetail = ['','']
 	global subjectCount
 	reviewDict = {
 		'id': subjectCount,
@@ -27,20 +41,26 @@ def getReviewInfo(review):
 		'orgin':routeDetail[0],
 		'destination' : routeDetail[1]
 	}
-	# reviewDict = {
-	# 	'id': subjectCount,
-	# 	'title': str(getTextFromTag(quote)),
-	# 	'rating':str(rating),
-	# 	'date':str(ratingDate),
-	# 	'comment':str(comment),
-	# 	'area':str(area),
-	# 	'class':str(seatClass),
-	# 	'orgin':str(routeDetail[0]),
-	# 	'orgin' : str(routeDetail[1])
-	# }
+	# print(subjectCount)
+	# print(str(getTextFromTag(quote)))
+	# print(str(rating))
+	# print(str(ratingDate))
+	# print(str(comment))
+	# print(str(area))
+	# print(str(seatClass))
+	# print(str(routeDetail[0]))
+	# print(str(routeDetail[1]))
+	# print('\n---\n')
 	global list2json
 	list2json.append(reviewDict)
 	subjectCount += 1
+
+def getDate(ratingDate):
+	try:
+		ratingDate = ratingDate['title'].encode('utf-8')
+	except:
+		ratingDate = getTextFromTag(ratingDate).split('的')[0]
+	return ratingDate
 
 def getRouteDetail(route):
 	routeInfo = str(route).split(' - ')
@@ -51,25 +71,24 @@ def getRouteDetail(route):
 def getTextFromTag(tag):
 	return tag.contents[0].encode('utf-8')
 
-page = 0
-subjectCount = 0
-list2json = []
-#Pagination
-URL = 'https://www.tripadvisor.com.tw/Airline_Review-d8729049-Reviews-Cheap-Flights-or'+str(page)+'0-China-Airlines#Reviews'
-#Url of China Airline in Trip Advisor
+def main():
+	generateURL()
+	global URL_List
+	for URL in URL_List:
+		time.sleep(1)
+		session = dryscrape.Session()
+		session.visit(URL)
+		response = session.body()
+		soup = bs4(response, 'html.parser')
+		reviews = soup.findAll("div", { "class" : "reviewSelector" })
+		for review in reviews:
+			getReviewInfo(review)
 
-r = requests.get(URL)
-soup = bs4(r.text, 'html.parser')
-reviews = soup.findAll("div", { "class" : "reviewSelector" })
-
-for review in reviews:
-	getReviewInfo(review)
-
-with open('data.json', 'w') as outfile:
-    json.dump(list2json, outfile, indent=4, sort_keys=True, ensure_ascii=False)
-
-# for review in reviews:
-# 	subjectCount += 1
-# 	print(review)
-# 	print("--\n")
-# 	print(subjectCount)
+if __name__ == '__main__':
+	subjectCount = 0
+	list2json = []
+	URL_List= []
+	main()
+	#Pagination
+	with open('data.json', 'w') as outfile:
+	    json.dump(list2json, outfile, indent=4, sort_keys=True, ensure_ascii=False)
