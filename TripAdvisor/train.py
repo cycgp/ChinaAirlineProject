d66a1361cc19a07752c6d7d201a8abb93805f540
CHIEN,CHING-YUAN <Henry@Henry-MBP.local>
2017-03-14 05:33:23 +0800

    complete gensim

--- TripAdvisor/train.py
+++ TripAdvisor/train.py
@@ -4,11 +4,11 @@ import logging
 def main():
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    sentences = word2vec.Text8Corpus("wiki_seg.txt")
-    model = word2vec.Word2Vec(sentences, size=250)
+    sentences = word2vec.Text8Corpus("data_seg.txt")
+    model = word2vec.Word2Vec(sentences, size=2)
 
     #保存模型，供日後使用
-    model.save_word2vec_format(u"med250.model.bin", binary=True)
+    model.wv.save_word2vec_format(u"med250.model.bin", binary=True)
 
     #模型讀取方式
     # model = word2vec.Word2Vec.load_word2vec_format("your_model.bin", binary=True)
